0a891d7c9beb34057435298d64e9320a99b254be
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-24 16:08:40 +0200

    Update GalacticFunctions.h

--- GalacticWar CLI game/GalacticFunctions.h
+++ GalacticWar CLI game/GalacticFunctions.h
@@ -1,17 +1,14 @@
 #ifndef __GALACTIC_F__ 
 #define __GALACTIC_F__
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <errno.h>
+#include "Galaxy.h"
 #define MAX_COMMAND 100
 #define COMMANDS 8
 #define NAME_SIZE 64
-#include "Galaxy.h"
-#include "Shields.h"
-
-#define DIE(suppose, error_message)									    \
+#define DIE(suppose, error_message)     							    \
 if (suppose) {														    \
     fprintf(stderr, "At line %d in file %s!\n", __LINE__, __FILE__);    \
     fprintf(stderr, "Errno value: %d with message: ", errno);		    \
@@ -24,7 +21,7 @@ if (suppose) {														    \
 void execute_command(const int command_index, char* command_line, galaxy_t **galaxy);
 
 // Add planet to the galaxy
-void ADD(const char* planet_name, const int planet_index, const int shields_number, galaxy_t** galaxy);
+void ADD(char* planet_name, const int planet_index, const int shields_number, galaxy_t** galaxy);
 
 // Throw planet into a blackhole
 void BLH(const int planet_index, galaxy_t **galaxy);
@@ -45,9 +42,12 @@ void COL(const int index_planet_1, const int index_planet_2, galaxy_t **galaxy);
 void ROT(const int planet_index, const char direction, int units, galaxy_t **galaxy);
 
 // Display planet info
-void SHW(const int planet_index, galaxy_t **galaxy);
+void SHW(int planet_index, galaxy_t **galaxy);
 
 // Return the index of the command from the command list or -1
 int return_index(const char* command, const char* command_list[]);
+
+// Print int list
+void dll_print_int_l(galaxy_t* list);
 
 #endif

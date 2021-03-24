75fcc3ccc5f3a93fac20a2bd628be400448ccbd0
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-24 16:08:54 +0200

    Update GalacticFunctions.c

--- GalacticWar CLI game/GalacticFunctions.c
+++ GalacticWar CLI game/GalacticFunctions.c
@@ -3,6 +3,7 @@
 // Execute the received command
 void execute_command(const int command_index, char* command_line, galaxy_t** galaxy)
 {
+	char received_command[4];
 	char * planet_name = NULL;
 	planet_name =  malloc(NAME_SIZE * sizeof(char));
 	DIE(!planet_name, "Didn't receive planet name correctly!\n");
@@ -17,35 +18,35 @@ void execute_command(const int command_index, char* command_line, galaxy_t** gal
 
 	switch(command_index) {
 		case 0:
-			sscanf(command_line,"%s%d%d",planet_name, &planet_index, &shields_number);
+			sscanf(command_line,"%s%s%d%d",received_command, planet_name, &planet_index, &shields_number);
 			ADD(planet_name, planet_index, shields_number, galaxy);
 			break;
 		case 1:
-			sscanf(command_line, "%d",&planet_index);
+			sscanf(command_line, "%s%d",received_command, &planet_index);
 			BLH(planet_index, galaxy);
 			break;
 		case 2:
-			sscanf(command_line, "%d%d%d",&planet_index,&shield_index,&upgrade_value);
+			sscanf(command_line, "%s%d%d%d",received_command,&planet_index,&shield_index,&upgrade_value);
 			UPG(planet_index, shield_index,upgrade_value, galaxy);
 			break;
 		case 3:
-			sscanf(command_line, "%d%d",&planet_index,&shield_value);
+			sscanf(command_line, "%s%d%d",received_command,&planet_index,&shield_value);
 			EXP(planet_index,shield_value, galaxy);
 			break;
 		case 4:
-			sscanf(command_line, "%d%d",&planet_index,&shield_index);
+			sscanf(command_line, "%s%d%d",received_command,&planet_index,&shield_index);
 			RMV(planet_index, shield_index, galaxy);
 			break;
 		case 5:
-			sscanf(command_line, "%d%d",&planet_index,&planet2_index);
+			sscanf(command_line, "%s%d%d",received_command,&planet_index,&planet2_index);
 			COL(planet_index, planet2_index, galaxy);
 			break;
 		case 6:
-			sscanf(command_line,"%d%c%d",&planet_index,&direction,&units);
+			sscanf(command_line,"%s%d%c%d",received_command,&planet_index,&direction,&units);
 			ROT(planet_index, direction, units, galaxy);
 			break;
 		case 7:
-			sscanf(command_line, "%d",&planet_index);
+			sscanf(command_line, "%s%d",received_command,&planet_index);
 			SHW(planet_index, galaxy);
 			break;
 		default:
@@ -55,49 +56,20 @@ void execute_command(const int command_index, char* command_line, galaxy_t** gal
 }
 
 // Add planet to the galaxy
-void ADD(const char* planet_name, const int planet_index, const int shields_number, galaxy_t** galaxy)
+void ADD(char* planet_name, const int planet_index, const int shields_number, galaxy_t** galaxy)
 {
-	printf("ADD\n");
-	return;
-	DIE(!*galaxy, "No galaxy here! Just a black hole!\n");
-	
-	if(!(*galaxy)->head) {
-
-		// Create a new empty new planet
-		planet_t* new_planet = NULL;
-		new_planet = malloc(sizeof(planet_t));
-		DIE(!new_planet,"Couldn't create new planet!\n");
-		new_planet->planetName = NULL;
-		new_planet->planetName = malloc(sizeof(NAME_SIZE));
-		DIE(!new_planet->planetName,"Couldn't add planet!\n");
-		new_planet->next = new_planet;
-		new_planet->prev = new_planet;
-		new_planet->shields_number = shields_number;
-		new_planet->distroyed_planets = 0;
-
-		// Create list of shields
-		new_planet->shieldHead = NULL;
-		shield_List* shieldHead = NULL;
-		shieldHead = init_head(sizeof(int));
-		DIE(!shieldHead,"Error creating list of shields!\n");
-		new_planet->shieldHead = shieldHead;
-		for(int i = 0; i < shields_number; i++){
-			int tmp = 1;
-			add_First(shieldHead, &tmp);
-		}
-		
-		
-		(*galaxy)->head = new_planet;
-		(*galaxy)->size++;
-		DIE(!(*galaxy)->head,"Couldn't create galaxy!\n");
-	}
+	dll_add_nth_node(*galaxy, planet_index, shields_number, planet_name);
+    printf("The planet %s has joined the galaxy.\n",((only_data_t*) (*galaxy)->head->data)->name);
 }
 
 // Throw planet into a blackhole
 void BLH(const int planet_index, galaxy_t **galaxy)
 {
-		printf("BLH\n");
-	return;
+	if(planet_index < 0) {
+		printf("Planet out of bounds!\n");
+		return;
+	}	
+	printf("The planet <planet name> has been eaten by the vortex.\n");
 }
 
 // Upgrade a planet
@@ -135,11 +107,80 @@ void ROT(const int planet_index, const char direction, int units, galaxy_t **gal
 	return;
 }
 
-// Display planet info
-void SHW(const int planet_index, galaxy_t **galaxy)
+// Print int list
+void
+dll_print_int_l(galaxy_t* list)
 {
-		printf("SHW\n");
-	return;
+    if(!list) {
+        fprintf(stderr,"No list to print!\n");
+        return;
+    }
+    else if(!list->head) {
+        fprintf(stderr,"List is empty!\n");
+        return;
+    }
+
+    galaxy_object* start = NULL;
+    start = list->head;
+    do {
+        printf("%d ",(*(int*)start->data));
+        start = start->next;
+    }
+    while(start != list->head);
+
+    printf("\n");
+}
+
+// Display planet info
+void SHW(int planet_index, galaxy_t **galaxy)
+{
+	if(!*galaxy || !(*galaxy)->head) {
+		fprintf(stderr,"No planet to display!\n");
+		return;
+	}
+	if(planet_index<0 || (unsigned int) planet_index >= (*galaxy)->galaxy_size) {
+		fprintf(stderr,"Planet out of bounds!\n");
+		return;
+	}
+	if(planet_index == 0) {
+		printf("NAME: %s\n",((only_data_t*)(*galaxy)->head->data)->name);
+		if(((only_data_t*)(*galaxy)->head->prev) != ((only_data_t*)(*galaxy)->head) && ((only_data_t*)(*galaxy)->head->next) != ((only_data_t*)(*galaxy)->head))
+			printf("CLOSEST: %s and %s\n",((only_data_t*)(*galaxy)->head->prev->data)->name,((only_data_t*)(*galaxy)->head->next->data)->name);
+		else if(((only_data_t*)(*galaxy)->head->prev) == ((only_data_t*)(*galaxy)->head) && ((only_data_t*)(*galaxy)->head->next) != ((only_data_t*)(*galaxy)->head))
+			printf("CLOSEST: %s\n",((only_data_t*)(*galaxy)->head->next->data)->name);
+		else if(((only_data_t*)(*galaxy)->head->prev) != ((only_data_t*)(*galaxy)->head) && ((only_data_t*)(*galaxy)->head->next) == ((only_data_t*)(*galaxy)->head))
+			printf("CLOSEST: %s\n",((only_data_t*)(*galaxy)->head->prev->data)->name);
+		else
+			printf("CLOSEST: none\n");
+		printf("SHIELDS: ");
+		dll_print_int_list(((only_data_t*)(*galaxy)->head->data)->shield);
+		printf("\nKILLED: %d\n",((only_data_t*)(*galaxy)->head->next->data)->destroyed_planets);
+	}
+	else {
+		int find_index = planet_index % (*galaxy)->galaxy_size;
+		galaxy_object* object_to_print = (*galaxy)->head;
+		for(int i=0;i < find_index;i++)
+			object_to_print = object_to_print->next;
+			
+		printf("NAME: %s\n",((only_data_t*)object_to_print->data)->name);
+
+		// TO FIX 
+		if(((only_data_t*)object_to_print->prev) != ((only_data_t*)object_to_print) && ((only_data_t*)object_to_print->next) != ((only_data_t*)object_to_print) && ((only_data_t*)object_to_print->prev) == ((only_data_t*)object_to_print->prev))
+			printf("CLOSEST: %s\n",((only_data_t*)object_to_print->prev->data)->name);
+		else if(((only_data_t*)object_to_print->prev) != ((only_data_t*)object_to_print) && ((only_data_t*)object_to_print->next) != ((only_data_t*)object_to_print))
+			printf("CLOSEST: %s and %s\n",((only_data_t*)object_to_print->prev->data)->name,((only_data_t*)object_to_print->next->data)->name);
+		else if(((only_data_t*)object_to_print->prev) == ((only_data_t*)object_to_print) && ((only_data_t*)object_to_print->next) != ((only_data_t*)object_to_print))
+			printf("CLOSEST: %s\n",((only_data_t*)object_to_print->next->data)->name);
+		else if(((only_data_t*)object_to_print->prev) != ((only_data_t*)object_to_print) && ((only_data_t*)object_to_print->next) == ((only_data_t*)object_to_print))
+			printf("CLOSEST: %s\n",((only_data_t*)object_to_print->prev->data)->name);
+		else
+			printf("CLOSEST: none\n");
+
+
+		printf("SHIELDS: ");
+		dll_print_int_list(((only_data_t*)object_to_print->data)->shield);
+		printf("\nKILLED: %d\n",((only_data_t*)object_to_print->next->data)->destroyed_planets);
+	}
 }
 
 // Return the index of the command from the command list or -1

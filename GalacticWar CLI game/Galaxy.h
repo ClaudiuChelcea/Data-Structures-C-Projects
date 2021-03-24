3f12b49e07cd54966830d64271e280b73617bcb4
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-24 16:09:14 +0200

    Update Galaxy.h

--- GalacticWar CLI game/Galaxy.h
+++ GalacticWar CLI game/Galaxy.h
@@ -1,9 +1,11 @@
-#ifndef __GALAXY_H_
-#define __GALAXY_H_
-#include <errno.h>
+#ifndef __GALAXY__H_
+#define __GALAXY__H_
 #include <stdio.h>
 #include <stdlib.h>
-#define DIE(suppose, error_message)									    \
+#include <errno.h>
+#include <string.h>
+#define NAME_SIZE 64
+#define DIE(suppose, error_message)     							    \
 if (suppose) {														    \
     fprintf(stderr, "At line %d in file %s!\n", __LINE__, __FILE__);    \
     fprintf(stderr, "Errno value: %d with message: ", errno);		    \
@@ -12,32 +14,74 @@ if (suppose) {														    \
     exit(errno);												    	\
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "Shields.h"
-#include <string.h>
-#define GARBAGE - 232343
-
-// Create planet structure
-typedef struct planet
+// Generic node
+typedef struct galaxy_object
 {
-	char * planetName;
-	struct planet* prev;
-	struct planet* next;
+    void* data;
+    struct galaxy_object *prev;
+	struct galaxy_object *next;
+} galaxy_object;
+
+// Galaxy, the list
+typedef struct galaxy_t
+{
+    galaxy_object* head;
+    unsigned int galaxy_size;
+} galaxy_t;
+
+// Data
+typedef struct only_data  {
+	char* name;
 	unsigned int shields_number;
-	unsigned int distroyed_planets;
-    shield_List* shieldHead;
-} planet_t;
+	unsigned int destroyed_planets;
+	galaxy_t* shield;
+} only_data_t;
 
+// Create the galaxy
+galaxy_t*
+dll_create();
 
-// Create the galaxy structure
-typedef struct galaxy
-{
-	unsigned int size;
-	struct planet* head;
-}  galaxy_t;
+// Add node to nth position
+void
+dll_add_nth_node_shield(galaxy_t* list, unsigned int n, const void* data);
 
-// Initialise galaxy
-galaxy_t* init_galaxy();
+// Add first shield
+void add_First(galaxy_t * list, void * new_data);
+
+// Return the nth node of the galaxy
+galaxy_object*
+dll_get_nth_node(galaxy_t* list, unsigned int n);
+
+// Add a node at the nth position
+void
+dll_add_nth_node(galaxy_t* list, int planet_index, int shields_number, char* planet_name);
+
+// Remove a node from the nth position
+void
+dll_remove_nth_node(galaxy_t** list, unsigned int n);
+
+// Get the size of the galaxy
+unsigned int
+dll_get_size(galaxy_t* list);
+
+// Throw the galaxy into a black hole(release the memory)
+void
+dll_free(galaxy_t** pp_list);
+
+// Print the galaxy with int values
+void
+dll_print_int_list(galaxy_t* list);
+
+// Print the galaxy with string values
+void
+dll_print_string_list(galaxy_t* list);
+
+// Print the galaxy to the left
+void
+dll_print_ints_left_circular(galaxy_object* start);
+
+// Print the galaxy to the right
+void
+dll_print_ints_right_circular(galaxy_object* start);
 
 #endif

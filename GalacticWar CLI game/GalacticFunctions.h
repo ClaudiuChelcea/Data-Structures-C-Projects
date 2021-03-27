e56a60058a1570695ec555c2508f19ad4f63a704
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-27 17:58:32 +0200

    Update GalacticFunctions.h

--- GalacticWar CLI game/GalacticFunctions.h
+++ GalacticWar CLI game/GalacticFunctions.h
@@ -10,13 +10,13 @@
 #define MAX_COMMAND 100
 #define COMMANDS 8
 #define NAME_SIZE 64
-#define DIE(suppose, error_message)\
-if (suppose) {                                                                  \
-    fprintf(stderr, "At line %d in file %s!\n", __LINE__, __FILE__);            \
-    fprintf(stderr, "Errno value: %d with message: ", errno);                   \
-    perror(error_message);                                                      \
-    printf("\n");                                                               \
-    exit(errno);                                                                \
+#define DIE(suppose, error_message)                                         \
+if (suppose) {                                                              \
+    fprintf(stderr, "At line %d in file %s!\n", __LINE__, __FILE__);        \
+    fprintf(stderr, "Errno value: %d with message: ", errno);               \
+    perror(error_message);                                                  \
+    printf("\n");                                                           \
+    exit(errno);                                                            \
 }
 
 // Execute the received command
@@ -32,24 +32,33 @@ void BLH(const int planet_index, galaxy_t ** galaxy);
 
 // Upgrade a planet
 void UPG(unsigned int planet_index,
-        unsigned int shield_index, unsigned
+    unsigned int shield_index, unsigned
     const int upgrade_value, galaxy_t ** galaxy);
 
+// Downgrade a planet
+int DOWN_UPG(unsigned int planet_index, unsigned int shield_index,
+    const int upgrade_value, galaxy_t ** galaxy);
+
+// Throw planet into a blackhole
+void BLH_implode(const int planet_index, galaxy_t ** galaxy);
+
 // Add a new shield to the planet
-void EXP(const int planet_index,
+void EXP(unsigned int planet_index,
     unsigned int shield_value, galaxy_t ** galaxy);
 
 // Remove a certain shield from a planet
-void RMV(const int planet_index,
-    const int shield_index, galaxy_t ** galaxy);
+void RMV(unsigned int planet_index,
+    unsigned int shield_index, galaxy_t ** galaxy);
 
 // Collide two planets
-void COL(const int index_planet_1,
-    const int index_planet_2, galaxy_t ** galaxy);
+void COL(unsigned int index_planet_1,
+    unsigned int index_planet_2, galaxy_t ** galaxy);
 
 // Rotate a planet (and it's shields)
-void ROT(const int planet_index,
+void ROT(unsigned int planet_index,
     const char direction, int units, galaxy_t ** galaxy);
+// Grow the kills of a planet
+void ADD_KILLS(unsigned int planet_index, galaxy_t ** galaxy);
 
 // Display planet info
 void SHW(int planet_index, galaxy_t ** galaxy);

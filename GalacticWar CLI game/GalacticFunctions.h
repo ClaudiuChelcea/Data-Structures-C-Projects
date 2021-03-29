292837bfec7629cb9472c148e7f2f6837fc5a0ad
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-03-29 22:55:45 +0300

    Add final version

--- GalacticWar CLI game/GalacticFunctions.h
+++ GalacticWar CLI game/GalacticFunctions.h
@@ -1,74 +1,46 @@
-#ifndef __GALACTIC_F__
-#define __GALACTIC_F__
+// Copyright Chelcea Claudiu-Marian
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#include "Galaxy.h"
+#include "GalacticFunctions.h"  // libraries, structures and function declarations
 
-#define MAX_COMMAND 100
-#define COMMANDS 8
-#define NAME_SIZE 64
-#define DIE(suppose, error_message)                                         \
-if (suppose) {                                                              \
-    printf("At line %d in file %s!\n", __LINE__, __FILE__);                 \
-    printf("Errno value: %d with message: ", errno);                        \
-    perror(error_message);                                                  \
-    printf("\n");                                                           \
-    exit(errno);                                                            \
+int main(void)
+{
+	// Get the number of inputs
+	int input_number = 0;
+	scanf("%d", &input_number);
+	getc(stdin);
+
+	// Create the command receiver and the command list
+	const char *command_list[COMMANDS] = {"ADD", "BLH", "UPG", "EXP",
+										  "RMV", "COL", "ROT", "SHW"};
+	char *command_line = NULL;
+	command_line = malloc(MAX_COMMAND * sizeof(char));
+	DIE(!command_line, "Couldn't start the program!\n");
+	char command[4];
+	int command_index = 0;
+	int global_size = 0;
+
+	// Create my galaxy
+	galaxy_t *my_galaxy = NULL;
+	my_galaxy = dll_create();
+	DIE(!my_galaxy, "Couldn't create the galaxy!\n");
+
+	// Get input and execute action correctly
+	for (int i = 0; i < input_number; i++) {
+		// Get the command
+		fgets(command_line, MAX_COMMAND, stdin);
+		sscanf(command_line, "%s", command);
+
+		// Get the index for the function that needs to be called
+		command_index = return_index(command, command_list);
+
+		// Execute the command
+		execute_command(command_index, command_line, &my_galaxy, &global_size);
+	}
+
+	// Release the memory
+	free(command_line);
+	command_line = NULL;
+	release_galaxy(&my_galaxy, global_size);
+
+	return 0;
 }
-
-// Execute the received command
-void execute_command(const int command_index, char * command_line, galaxy_t ** galaxy, int* global_size);
-
-// Add planet to the galaxy
-void ADD(char * planet_name, const int planet_index,
-         const int shields_number, galaxy_t ** galaxy, int* global_size);
-
-// Throw planet into a blackhole
-void BLH(const int planet_index, galaxy_t ** galaxy, int* global_size);
-
-// Upgrade a planet
-void UPG(unsigned int planet_index, unsigned int shield_index, unsigned
-         const int upgrade_value, galaxy_t ** galaxy);
-
-// Downgrade a planet
-int DOWN_UPG(unsigned int planet_index, unsigned int shield_index,
-             const int upgrade_value, galaxy_t ** galaxy);
-
-// Throw planet into a blackhole
-void BLH_implode(const int planet_index, galaxy_t ** galaxy, int* global_size);
-
-// Add a new shield to the planet
-void EXP(unsigned int planet_index,
-         unsigned int shield_value, galaxy_t ** galaxy);
-
-// Remove a certain shield from a planet
-void RMV(unsigned int planet_index,
-         unsigned int shield_index, galaxy_t ** galaxy);
-
-// Collide two planets
-void COL(unsigned int index_planet_1,
-         unsigned int index_planet_2, galaxy_t ** galaxy, int* global_size);
-
-// Rotate a planet (and it's shields)
-void ROT(unsigned int planet_index,
-         const char direction, int units, galaxy_t ** galaxy);
-
-// Grow the kills of a planet
-void ADD_KILLS(unsigned int planet_index, galaxy_t ** galaxy);
-
-// Display planet info
-void SHW(int planet_index, galaxy_t ** galaxy);
-
-// Return the index of the command from the command list or -1
-int return_index(const char * command, const char * command_list[]);
-
-// Print int list
-void dll_print_int_l(galaxy_t * list);
-
-// Release the memory
-void release_galaxy(galaxy_t ** galaxy, int global_size);
-
-#endif

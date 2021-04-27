95c004c0fb5914bcc41f60d7416dc89624da3738
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-04-28 00:04:15 +0300

    Update client.h

--- LOAD BALANCER/client.h
+++ LOAD BALANCER/client.h
@@ -1,19 +1,19 @@
 /* Copyright Chelcea Claudiu-Marian */
 
-#ifndef __CLIENT__H_
-#define __CLIENT__H_
+#ifndef CHECKER_CLIENT_H_
+#define CHECKER_CLIENT_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 // Execute command based on input
-void apply_requests(FILE * input_file);
+void apply_requests(FILE *input_file);
 
 // Get element key from request
-void get_key(char * key, char * request);
+void get_key(char *key, char *request);
 
 // Get element value from request
-void get_key_value(char * key, char * value, char * request);
+void get_key_value(char *key, char *value, char *request);
 
-#endif
+#endif  // CHECKER_CLIENT_H_

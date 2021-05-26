9c747b0600d60697f43ec9cc9220a3b874d2cab3
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-05-26 23:54:17 +0300

    Added skel

---
+++ Rope Data Structure/rope.c
@@ -0,0 +1,193 @@
+/* Copyright Chelcea Claudiu-Marian & Brinzan  Darius-Ionut */
+#include "./rope.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#define EMPTY ""
+
+// Create a node with the given text
+RopeNode* makeRopeNode(const char* str)
+{
+    // Allocate memory for the node
+    RopeNode* new_Node = NULL;
+    new_Node = calloc(1, sizeof(*new_Node));
+    DIE(!new_Node, "Couldn't create new node!");
+
+    // FIll node's fields
+    new_Node->left = NULL;
+    new_Node->right = NULL;
+    new_Node->str = calloc(NODE_DATA_MAX_LENGTH, sizeof(char));
+    memmove(new_Node->str, str, strlen(str));
+    DIE(!new_Node->str, "Couldn't create new node!");
+    new_Node->weight = strlen(new_Node->str);
+
+    return new_Node;
+}
+
+RopeTree* makeRopeTree(RopeNode* root)
+{
+    // Create tree
+    RopeTree* new_Tree = NULL;
+    new_Tree = calloc(1, sizeof(*new_Tree));
+    DIE(!new_Tree, "Couldn't create tree!");
+
+    // Assign tree's root
+    new_Tree->root = root;
+
+    return new_Tree;
+}
+
+
+void printRopeNode(RopeNode* rn) {
+    if (!rn)
+        return;
+
+    if (!(rn->left) && !(rn->right)) {
+        printf("%s", rn->str);
+        return;
+    }
+
+    printRopeNode(rn->left);
+    printRopeNode(rn->right);
+}
+
+void printRopeTree(RopeTree* rt) {
+    if (rt && rt->root) {
+        printRopeNode(rt->root);
+        printf("%s", "\n");
+    }
+}
+
+void debugRopeNode(RopeNode* rn, int indent) {
+    if (!rn)
+        return;
+
+    for (int i = 0; i < indent; ++i)
+        printf("%s", " ");
+
+    if (!strcmp(rn->str, EMPTY))
+        printf("# %d\n", rn->weight);
+    else
+        printf("%s %d\n", rn->str, rn->weight);
+
+    debugRopeNode(rn->left, indent+2);
+    debugRopeNode(rn->right, indent+2);
+}
+
+
+int getTotalWeight(RopeNode* rt) {
+    if (!rt)
+        return 0;
+
+    return rt->weight + getTotalWeight(rt->right);
+}
+
+
+RopeTree* concat(RopeTree* rt1, RopeTree* rt2)
+{
+    // Create new root node
+    RopeNode* new_Node = makeRopeNode(EMPTY);
+
+    // Assing new values
+    new_Node->right = rt2;
+    new_Node->left = rt1;
+    new_Node->weight = getTotalWeight(rt1);    
+}
+
+char indexRope(RopeTree* rt, int idx) {
+    // TODO 2. Index - 10p
+}
+
+
+char* search(RopeTree* rt, int start, int end) {
+    // TODO 3. Search - 20p
+}
+
+SplitPair split(RopeTree* rt, int idx) {
+    // TODO 4. Split - 20p
+}
+
+RopeTree* insert(RopeTree* rt, int idx, const char* str) {
+    // TODO 5. Insert - 5p
+}
+
+RopeTree* delete(RopeTree* rt, int start, int len) {
+    // TODO 6. Delete - 5p
+}
+
+// FINAL 10p -> complex test involving all operations
+char *strdup(const char *s);
+
+const char* strs[] = {"Ana", "are", "mere", "legume", "o", "mandarine"};
+
+RopeNode* rnallocated[1000];
+RopeTree* rtallocated[1000];
+int numrn = 0;
+int numrt = 0;
+
+RopeNode * checkerMakeRopeNode(const char *str) {
+   rnallocated[numrn] = makeRopeNode(str);
+   return rnallocated[numrn++];
+}
+
+RopeTree * checkerMakeRopeTree(RopeNode * rn) {
+   rtallocated[numrt] = makeRopeTree(rn);
+   return rtallocated[numrt++];
+}   
+
+int main() {
+
+    RopeNode *rn1, *rn2, *rn3, *rn4, *rn5, *rn6, *rn7;
+    RopeTree *rt, *rt1, *rt2;
+
+    rn1 = checkerMakeRopeNode(strdup("abc"));
+    rn2 = checkerMakeRopeNode(strdup("de"));
+    rn3 = checkerMakeRopeNode(strdup("fghi"));
+    rn4 = checkerMakeRopeNode(strdup("jklmn"));
+    rn5 = checkerMakeRopeNode(strdup(EMPTY));
+    rn6 = checkerMakeRopeNode(strdup(EMPTY));
+    rn7 = checkerMakeRopeNode(strdup(EMPTY));
+
+    rn5->left = rn1;
+    rn5->right = rn2;
+    rn5->weight = 3;
+
+    rn6->left = rn3;
+    rn6->right = rn4;
+    rn6->weight = 4;
+
+    rn7->left = rn5;
+    rn7->right = rn6;
+    rn7->weight = 5;
+
+    rt = checkerMakeRopeTree(rn7);
+
+    for (unsigned int i = 0; i < sizeof(strs) / sizeof(const char*); ++i) {
+        rt1 = concat(checkerMakeRopeTree(checkerMakeRopeNode(strdup(strs[i]))), rt);
+        printRopeTree(rt1);
+        rt2 = concat(rt, checkerMakeRopeTree(checkerMakeRopeNode(strdup(strs[i]))));
+        printRopeTree(rt2);
+        printf("\n");
+
+        free((void*)rt1->root->str);
+        free(rt1->root);
+        free(rt1);
+
+        free((void*)rt2->root->str);
+        free(rt2->root);
+        free(rt2);
+    }
+
+    for (int i = 0; i < numrn; i++) {
+        free((void*)rnallocated[i]->str);
+        free(rnallocated[i]);
+    }
+
+    for (int i = 0; i < numrt; i++) {
+        free(rtallocated[i]);
+    }
+
+    return 0;
+}

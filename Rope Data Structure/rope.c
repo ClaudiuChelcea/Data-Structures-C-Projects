64103722377e0407dc8f30dd3e6152422583e736
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-06-02 02:29:51 +0300

    fix cs

--- Rope Data Structure/rope.c
+++ Rope Data Structure/rope.c
@@ -122,7 +122,6 @@ char __indexRope(RopeNode* root, int index)
 {
     // Search by index
     if (!root -> left && !root -> right) {
-
         // Return character if we are on a leaf
         return root->str[index];
     }
@@ -193,21 +192,21 @@ char* search(RopeTree* rt, int start, int end)
 RopeNode* return_wanted_leaf(RopeNode* root, int index)
 {
     // If we can't go deeper, return the node
-    if(!root->left && !root->right) {
+    if (!root->left && !root->right) {
         return root;
     }
 
     // Go down the tree recursively
-    if(root->weight >= index) {
-        return return_wanted_leaf(root->left,index);
-    } else if(root->weight < index) {
-        return return_wanted_leaf(root->right,index - root->weight);
+    if (root->weight >= index) {
+        return return_wanted_leaf(root->left, index);
+    } else if (root->weight < index) {
+        return return_wanted_leaf(root->right, index - root->weight);
     }
     return NULL;
 }
 
 // Allocate string and copy contents
-char* new_string(int start, int stop, char* source) 
+char* new_string(int start, int stop, char* source)
 {
     // Receive the part of the string
     char* my_string = NULL;
@@ -216,7 +215,7 @@ char* new_string(int start, int stop, char* source)
 
     // Copy content in the string
     int position = 0;
-    for(int i = start; i< stop; i++)
+    for (int i = start; i< stop; i++)
         my_string[position++] = source[i];
 
     // Return the new string
@@ -237,26 +236,27 @@ void splitNode(RopeNode* node, int index)
 
 // Inorder traversal to store in the 'nodes_to_move' variable all nodes
 // than are after index and therefore in the right subtree
-void get_extra_nodes(RopeNode** nodes_to_move, RopeNode* root, int *nr_elements, bool* break_condition, int index)
+void get_extra_nodes(RopeNode** nodes_to_move, RopeNode* root,
+                     int *nr_elements, bool* break_condition, int index)
 {
     // Break recursivity
-    if(!root)
+    if (!root)
         return;
 
     // If we are on the leaf
-    if(!root->left && !root->right) {
+    if (!root->left && !root->right) {
         // If we have broken down the leaf enough so that the index
         // matches the root's weight, set break_condition to 1
         // to start returning all other nodes from now on in nodes_to_move
         // variable
-        if(index == root->weight) {
+        if (index == root->weight) {
             *break_condition = 1;
             return;
         }
-        // Else split the node in two nodes
-        // Example: From abc we will have node->left = 'a' 
-        // and node->right =  'bc' while the current node stays the same
         else {
+            // Else split the node in two nodes
+            // Example: From abc we will have node->left = 'a' 
+            // and node->right =  'bc' while ( the current node stays the same
             splitNode(root,index);
             index = root->left->weight;
         }
@@ -264,9 +264,9 @@ void get_extra_nodes(RopeNode** nodes_to_move, RopeNode* root, int *nr_elements,
 
     // Inorder traversal until we find node at the 'index' position
     // only as long as the break_condition has not been fullfilled
-    if(!(*break_condition)) {
+    if (!(*break_condition)) {
         // Go left if the weight is bigger than our index
-        if(index <= root->weight) {
+        if (index <= root->weight) {
             get_extra_nodes(nodes_to_move, root->left, nr_elements, break_condition, index);
         }
         // Otherwise go right
@@ -278,7 +278,7 @@ void get_extra_nodes(RopeNode** nodes_to_move, RopeNode* root, int *nr_elements,
     // While going up the tree from the recursivity
     // Put all the right nodes that don't fit in the index
     // in the vector intended to move them to the right subtree
-    if(root->weight >= index)  {
+    if (root->weight >= index)  {
         // Save element
         nodes_to_move[*nr_elements] = root->right;
         
@@ -294,7 +294,7 @@ void get_extra_nodes(RopeNode** nodes_to_move, RopeNode* root, int *nr_elements,
 void __free_Nodes(RopeNode** root)
 {
     // Stop recursivity
-    if(!(*root))
+    if (!(*root))
         return;
         
     // Recursive calls on all the nodes of the tree
@@ -329,7 +329,7 @@ SplitPair split(RopeTree* rt, int idx)
     SplitPair my_pair = __make_pair(new_Tree->root);
 
     // If the index is '0'
-    if(idx == 0) {
+    if (idx == 0) {
         // We don't need to change anything, just return the already built tree
         free(new_Tree);
         return my_pair;
@@ -345,15 +345,14 @@ SplitPair split(RopeTree* rt, int idx)
     get_extra_nodes(nodes_to_move, my_pair.left, &nr_elements, &break_condition, idx);
 
     // Move all elements from the vector to the right tree
-    for(int i=0;i<nr_elements;++i) {
+    for (int i=0;i<nr_elements;++i) {
         my_pair.right = node_concat(my_pair.right,nodes_to_move[i]);
     }
-
     // Release memory
     free(nodes_to_move);
     free(new_Tree);  
-
-    // Return the two newly formed trees  
+    
+    // Return the two newly formed trees
     return my_pair;
 }
 
@@ -361,9 +360,7 @@ SplitPair split(RopeTree* rt, int idx)
 RopeTree* insert(RopeTree* rt, int idx, const char* str)
 {
     // Split the current string at the 'idx' position
-    SplitPair pair = split(rt, idx);
-
-    // Concatenate the left string with the new string and with the right string
+    SplitPair pair = split(rt, idx); // Concatenate left + new + right
     return concat(concat(makeRopeTree(pair.left),
                   makeRopeTree(makeRopeNode(str))), 
                   makeRopeTree(pair.right));
@@ -374,7 +371,6 @@ RopeTree* delete(RopeTree* rt, int start, int len)
 {
     // Split the string at the 'start' position
     SplitPair pair1 = split(rt, start);
-    
     // Split the right string again
     SplitPair pair2 = split(makeRopeTree(pair1.right), start + len);
 
@@ -382,3 +378,4 @@ RopeTree* delete(RopeTree* rt, int start, int len)
     return concat(makeRopeTree(pair1.left),
                   makeRopeTree(pair2.right));
 }
+
